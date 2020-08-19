9f55e965d70804c94c59026f9b1b855be188bc3e
oyjuffer <oyjuffer@outlook.com>
2020-08-19 16:20:09 +0300

    Main Game File

---
+++ characters.py
@@ -0,0 +1,81 @@
+# Import Libraries
+import pygame
+import random
+import os
+from time import sleep
+
+# Import Game Files
+from items import *
+
+
+### PLAYER CLASS ###
+class Character:
+    def __init__(self, name, level, strength, intelligence, constitution, equipped_weapon):
+        self.name = name
+        self.level = level
+        self.difficulty = 1
+
+        self.strength = strength
+        self.strength_reset = 0
+        self.physical_damage = 0
+        self.min_physical_damage = 0
+        self.max_physical_damage = 0
+
+        self.intelligence = intelligence
+        self.intelligence_reset = 0
+        self.magic_damage = 0
+        self.min_magic_damage = 0
+        self.max_magic_damage = 0
+
+        self.constitution = constitution
+        self.constitution_reset = 0
+        self.health = 0
+        self.max_health = 0
+
+        self.skill_points = 1
+        self.skill_points_reset = 0
+
+        self.damage = 0
+        self.missed = False
+
+        self.equipped_weapon = equipped_weapon
+        self.equipped_armour = none
+        self.equipped_token = none
+
+        self.inventory_weapon = [sword, mace, rune_sword, axe, vorpal_sword, staff]
+        self.inventory_armour = [chain_mail, plate_mail]
+        self.inventory_token = [health_ring, ring_of_intelligence, spikey_ring]
+
+    def convert_stats(self):
+        self.physical_damage = (
+                    self.strength + self.equipped_weapon.strength + self.equipped_armour.strength + self.equipped_token.strength)
+        self.magic_damage = (
+                    self.intelligence + self.equipped_weapon.intelligence + self.equipped_armour.intelligence + self.equipped_token.intelligence)
+        self.max_health = (
+                                      self.constitution + self.equipped_weapon.constitution + self.equipped_armour.constitution + self.equipped_token.constitution) * 10 + 100
+
+        self.min_physical_damage = self.equipped_weapon.physical_damage_note[0] + \
+                                   self.equipped_armour.physical_damage_note[0] + \
+                                   self.equipped_token.physical_damage_note[0]
+        self.max_physical_damage = self.equipped_weapon.physical_damage_note[1] + \
+                                   self.equipped_armour.physical_damage_note[1] + \
+                                   self.equipped_token.physical_damage_note[1]
+
+        self.min_magic_damage = self.equipped_weapon.magic_damage_note[0] + self.equipped_armour.magic_damage_note[0] + \
+                                self.equipped_token.magic_damage_note[0]
+        self.max_magic_damage = self.equipped_weapon.magic_damage_note[1] + self.equipped_armour.magic_damage_note[1] + \
+                                self.equipped_token.magic_damage_note[1]
+
+    def attack(self, target):
+        self.damage_done = random.randint(self.min_physical_damage, self.max_physical_damage) + self.physical_damage
+        return self.damage_done
+
+    def magic(self, ability, target):
+        pass
+
+    def inventory(self):
+        self.inventory = []
+
+
+player = Character("Lorther", 1, 10, 1, 1, none)
+npc_dustin_echos = Character("Dustin Echos", 2, 2, 2, 2, vorpal_sword)
