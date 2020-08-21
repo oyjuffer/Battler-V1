4fa4750f81849f69db7310eff645c99e73f6910f
oyjuffer <oyjuffer@outlook.com>
2020-08-21 15:56:07 +0300

    added a new character

--- characters.py
+++ characters.py
@@ -5,10 +5,11 @@ import os
 from time import sleep
 
 # Import Game Files
+from abilities import *
 from items import *
 
 
-### PLAYER CLASS ###
+### CHARACTER CLASS ###
 class Character:
     def __init__(self, name, level, strength, intelligence, constitution, equipped_weapon):
         self.name = name
@@ -35,24 +36,23 @@ class Character:
         self.skill_points = 1
         self.skill_points_reset = 0
 
-        self.damage = 0
-        self.missed = False
+        self.damage             = 0
+        self.missed             = False
 
-        self.equipped_weapon = equipped_weapon
-        self.equipped_armour = none
-        self.equipped_token = none
+        self.equipped_weapon    = equipped_weapon
+        self.equipped_armour    = none
+        self.equipped_token     = none
 
-        self.inventory_weapon = [sword, mace, rune_sword, axe, vorpal_sword, staff]
-        self.inventory_armour = [chain_mail, plate_mail]
-        self.inventory_token = [health_ring, ring_of_intelligence, spikey_ring]
+        self.inventory_weapon   = [sword, mace, rune_sword, axe, vorpal_sword, staff]
+        self.inventory_armour   = [chain_mail, plate_mail]
+        self.inventory_token    = [health_ring, ring_of_intelligence, spikey_ring]
+
+        self.magic_abilities    = [fire1]
 
     def convert_stats(self):
-        self.physical_damage = (
-                    self.strength + self.equipped_weapon.strength + self.equipped_armour.strength + self.equipped_token.strength)
-        self.magic_damage = (
-                    self.intelligence + self.equipped_weapon.intelligence + self.equipped_armour.intelligence + self.equipped_token.intelligence)
-        self.max_health = (
-                                      self.constitution + self.equipped_weapon.constitution + self.equipped_armour.constitution + self.equipped_token.constitution) * 10 + 100
+        self.physical_damage    = (self.strength + self.equipped_weapon.strength + self.equipped_armour.strength + self.equipped_token.strength)
+        self.magic_damage       = (self.intelligence + self.equipped_weapon.intelligence + self.equipped_armour.intelligence + self.equipped_token.intelligence)
+        self.max_health         = (self.constitution + self.equipped_weapon.constitution + self.equipped_armour.constitution + self.equipped_token.constitution) * 10 + 100
 
         self.min_physical_damage = self.equipped_weapon.physical_damage_note[0] + \
                                    self.equipped_armour.physical_damage_note[0] + \
