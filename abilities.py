5f81c9f42ff8dcf3ffb1e84781fb7f314a091ff2
oyjuffer <oyjuffer@outlook.com>
2020-08-21 15:55:37 +0300

    prep for magic system

--- abilities.py
+++ abilities.py
@@ -7,7 +7,16 @@ from time import sleep
 # Import Game Files
 ###
 
-### ITEM CLASS ###
+### ABILITY CLASS ###
+class Ability:
+	def __init__(self, name, physical_damage, magic_damage, healing, miss, other):
+		self.name 				= name
+		self.physical_damage 	= physical_damage
+		self.magic_damage 		= magic_damage
+		self.healing			= healing 
+		self.miss 				= miss
 
 
-print("hello")
+
+fire1			= Ability("Fire I", 0 , random.randint(1, 6), 0, 0, None)
+fire2			= Ability("Fire II", 0 , random.randint(6, 12), 0, 0, None)
